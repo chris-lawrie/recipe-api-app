5793c43768d0cc0bce527525e22f4221c18cabcd
Chris Lawrie <clawrie2020@gmail.com>
2024-01-29 18:07:33 -0500

    setup some example tests

---
+++ app/app/tests.py
@@ -0,0 +1,21 @@
+"""
+Sample tests
+"""
+from django.test import SimpleTestCase
+
+from app import calc
+
+
+class CalcTests(SimpleTestCase):
+    """
+    Test the calc module
+    """
+
+    def test_add_numbers(self):
+        result = calc.add(5,6)
+        self.assertEqual(result, 11)
+    
+    def test_subtract_numbers(self):
+        result = calc.subtract(10, 15)
+        self.assertEqual(result, 5)
+
